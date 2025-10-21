0a5fe6bf554e60844b438ae9ec4fb6ae38952216
Lucas Ivy <lucasjamesivy@gmail.com>
2025-10-20 19:09:19 -0500

    new approach pt1

--- ImageMorph/morpher.py
+++ ImageMorph/morpher.py
@@ -13,13 +13,12 @@ def get_dist(point1,point2):
 
 def get_compatibility(pixel1,pixel2):
     if get_dist(pixel1[0],pixel2[0]) != 0:
-        return ((255 - get_similarity_of_colors(pixel1[1],pixel2[1])))/(get_dist(pixel1[0],pixel2[0]))
+        return ((255 - get_similarity_of_colors(pixel1[1],pixel2[1])))/(get_dist(pixel1[0],pixel2[0])) #squared
     else:
-        return (255 - get_similarity_of_colors(pixel1[1],pixel2[1]))/1
+        return (255 - get_similarity_of_colors(pixel1[1],pixel2[1]))/0.5
 
-
-image1 = Image.open("ImageMorph/fall_256.png")
-image2 = Image.open("ImageMorph/night_256.png")
+image1 = Image.open("ImageMorph/fall_32.png")
+image2 = Image.open("ImageMorph/night_32.png")
 
 data1 =  np.asarray(image1)
 data2 = np.asarray(image2)
@@ -27,9 +26,8 @@ data2 = np.asarray(image2)
 size = len(data1)
 
 print("Counting 1")
-count = 0
-for i in range(size**2 ):
-    count += size**2 - i
+
+count = int((size**2)*((1+(size**2))//2))
 
 pool1 = list()
 pool2 = list()
@@ -41,28 +39,138 @@ for x in range(len(data1)):
     for y in range(len(data1[0])):
         pool1.append([[x,y],data1[x][y]])
         
-n_pool1 = list()
-for i in range(len(pool1)):
-    n_pool1.append(pool1.pop(np.random.randint(0,len(pool1))))
-pool1 = n_pool1
+#Shuffle
+# n_pool1 = list()
+# for i in range(len(pool1)):
+#     n_pool1.append(pool1.pop(np.random.randint(0,len(pool1))))
+# pool1 = n_pool1
         
 print("Pooling 2") 
 for x in range(len(data2)):
     for y in range(len(data2[0])):
         pool2.append([[x,y],data2[x][y]])
         
-print("Pairing") # MOST IMPORTANT PART
+
+print("Initial Pairing") 
 percent = 0
+max_dist = size//2
+
 for i,pixel1 in enumerate(pool1):
-    print(f"{(percent * 100)/(count):.3f}")
-    percent += len(pool2)
+    print(f"{(i * 100)/(len(pool1)):.3f}")
     
-    compatibilities = list()
-    for pixel2 in pool2:
-        compatibilities.append([get_compatibility(pixel1,pixel2),pixel2])
-    compatibilities.sort()
-    pairs.append([pixel1,compatibilities[0][1]])
-    del pool2[pool2.index(compatibilities[0][1])]
+    best_compatibility = -1
+    most_compatible_pixel = pool2[0]
+    
+    for x in range(pixel1[0][0] - (max_dist//2) + 1,pixel1[0][0] + (max_dist//2) - 1):
+        if x < 0 or x >= size:
+            continue
+        for y in range(pixel1[0][1] - max_dist//2 + 1,pixel1[0][1] + max_dist//2 - 1):
+            if y < 0 or y >= size:
+                continue
+            pixel2 = pool2[y + x * size]
+            compatibility = get_compatibility(pixel1,pixel2)
+            if compatibility > best_compatibility:
+                most_compatible_pixel = pixel2
+                best_compatibility = compatibility 
+    pairs.append([pixel1,most_compatible_pixel])
+
+
+print("Resvoling Conflicts") # MOST IMPORTANT PART
+conflicts = list()
+available_pool = list()
+
+for pixel2 in pool2:
+    contested = 0
+    paired = False
+    temp = None
+    for pair in pairs:    
+        if pair[1] == pixel2:
+            paired = True
+            if contested == 0:
+                temp = pair
+            if contested == 1:
+                conflicts.append(temp)
+                conflicts.append(pair)
+            if contested > 1:
+                conflicts.append(pair)
+            contested += 1
+    if not paired:
+        available_pool.append(pixel2)
+# conflicts should be in order like -> [0,0,0,0,1,1,2,2,2]
+
+print(f"{100.0 - (len(conflicts)/(64**2))}f.2f")
+
+
+to_review = list()
+
+while(len(conflicts) != 0):
+    i = 0
+    print("Resvoling Conflicts 1", len(conflicts))
+    while(i < len(conflicts)):
+        c_pixel = conflicts[i][1]
+        c_contestants = list()
+
+        while i < len(conflicts) and conflicts[i][1] == c_pixel:
+            c_contestants.append(conflicts[i])
+            i += 1
+        
+        best_compatibility = -1
+        most_compatible_contestant = c_contestants[0]
+        
+        #give the most deserving contestant c_pixel
+            # c_contestants[0] is the pixel to be given best pair
+            # c_contestants[1] is the pixel that they share
+        for contestant in c_contestants:
+            compatibility = get_compatibility(contestant[0],contestant[1])
+            if compatibility > best_compatibility:
+                best_compatibility = compatibility
+                most_compatible_contestant = contestant
+        #Remove locked in pairs
+        del c_contestants[c_contestants.index(most_compatible_contestant)]
+        #give the rest the optimal avaibale pixels
+        for contestant in c_contestants:
+            best_compatibility = -1
+            most_compatible_pixel = None
+            for a_pixel in available_pool:
+                compatibility = get_compatibility(contestant[0],a_pixel)
+                if compatibility > best_compatibility:
+                    best_compatibility = compatibility
+                    most_compatible_pixel = a_pixel
+            to_review.append([c_contestants[0],most_compatible_pixel])
+    
+    #add none conlicts back to source
+    #add any conflicts back to conlicts and redo
+  
+    print("Resvoling Conflicts 2", len(conflicts), len(to_review))
+
+    conflicts = list()
+    sucess = 0
+    for pair1 in to_review:
+        contested = 0
+        for pair2 in to_review:  
+            pixel2 = pair2[1]
+            if pair1[1] == pixel2:
+                if contested >= 1:
+                    conflicts.append(pair)
+                contested += 1
+        if contested == 1: # if not contested
+            sucess += 1
+            for source_pair in pairs:
+                if source_pair[0] == pair1:
+                    source_pair = pair1 #add to source
+                    del to_review[to_review.index(pair1)] #no need to review
+                    del available_pool[available_pool.index(pair1[1])] # pixel is no longer available
+                    break
+    print("Resvoling Conflicts 3: shuld be less", len(conflicts),sucess)
+    to_review = list()
+        
+            
+    
+    
+        
+
+        
+        
     
     #NAIVE APPRROACH ^^^ Noisey outputs
     #Better one: pair every pixel with its most opimal ones, deal with contestions recurisvly
