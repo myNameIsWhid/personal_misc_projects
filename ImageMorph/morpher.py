03e940651e8dcc3aab1df327ae1bf27f347c4182
Lucas Ivy <lucasjamesivy@gmail.com>
2025-10-21 19:28:23 -0500

    Morpher pt2.2

--- ImageMorph/morpher.py
+++ ImageMorph/morpher.py
@@ -2,11 +2,12 @@ from PIL import Image
 import numpy as np
 import math
 from progress.bar import Bar 
+import cv2
 
 def get_similarity_of_colors(color1,color2):
     avg = 0.0
     for i in range(3):
-        avg += abs(color1[i]-color2[i]) 
+        avg += abs(float(color1[i])-float(color2[i])) 
     return int(avg/3.0)
 
 def get_dist(point1,point2):
@@ -15,17 +16,21 @@ def get_dist(point1,point2):
 def get_compatibility(pixel1,pixel2):
     color_weight = 1
     if get_dist(pixel1[0],pixel2[0]) != 0:
-        return (color_weight * ((255 - get_similarity_of_colors(pixel1[1],pixel2[1]))))
+        return (color_weight * ((255 - get_similarity_of_colors(pixel1[1],pixel2[1]))))#/get_dist(pixel1[0],pixel2[0])
     else:
         return (color_weight * (255 - get_similarity_of_colors(pixel1[1],pixel2[1])))
 
-image1 = Image.open("ImageMorph/fall_128.png")
-image2 = Image.open("ImageMorph/night_128.png")
+image1 = Image.open("ImageMorph/fall_1024.png")
+image2 = Image.open("ImageMorph/night_1024.png")
+
+
 
 data1 =  np.asarray(image1)
 data2 = np.asarray(image2)
 
 size = len(data1)
+
+video = cv2.VideoWriter("ImageMorph/output.avi", cv2.VideoWriter_fourcc(*'DIVX'), 120, (size * 2 , size))
 
 print("Counting 1")
 
@@ -54,11 +59,35 @@ print("Initial Pairing")
 
 max_dist = 4
 
-
-def make_image():
-    print("Creating")
+def video_add_frame():
     morph_data1 = np.empty((len(data1),len(data2),3),dtype=np.uint8)
     morph_data1.fill(0)  
+    for pair in pairs:
+        pixel1 = pair[0]
+        pixel2 = pair[1]
+        for i in range(3):
+            morph_data1[pixel2[0][0]][pixel2[0][1]][i] = pixel1[1][2 - i]
+
+    morph_data2 = np.empty((len(data1),len(data2),3),dtype=np.uint8)
+    morph_data2.fill(0)  
+    for pair in pairs:
+        pixel1 = pair[0]
+        pixel2 = pair[1]
+        for i in range(3):
+            morph_data2[pixel1[0][0]][pixel1[0][1]][i] = pixel2[1][2 - i]
+            
+    morph1 = Image.fromarray(morph_data1)
+    morph2 = Image.fromarray(morph_data2)
+    combined_img = Image.new('RGB', (len(morph_data1) * 2, len(morph_data1)), (255, 255, 255)) 
+    combined_img.paste(morph1, (0, 0)) # Paste img1 at top-left
+    combined_img.paste(morph2 , (len(morph_data1), 0)) # Paste img1 at top-left
+    video.write(np.asarray(combined_img))
+    
+
+def make_image(kind = 2):
+    print("Creating")
+    morph_data1 = np.empty((len(data1),len(data2),3),dtype=np.uint8)
+    morph_data1.fill(255)  
     for pair in pairs:
         pixel1 = pair[0]
         pixel2 = pair[1]
@@ -75,19 +104,27 @@ def make_image():
             
     morph1 = Image.fromarray(morph_data1)
     morph2 = Image.fromarray(morph_data2)
-    combined_img = Image.new('RGB', (len(morph_data1) * 3, len(morph_data1) * 2), (255, 255, 255)) 
+    if(kind == 2):
+        combined_img = Image.new('RGB', (len(morph_data1) * 3, len(morph_data1) * 2), (255, 255, 255)) 
 
-    combined_img.paste(image1, (0, 0)) # Paste img1 at top-left
-    combined_img.paste(morph1 , (len(morph_data1), 0)) # Paste img1 at top-left
-    combined_img.paste(image2, (len(morph_data1) * 2, 0)) # Paste img2 next to img1
+        combined_img.paste(image1, (0, 0)) # Paste img1 at top-left
+        combined_img.paste(morph1 , (len(morph_data1), 0)) # Paste img1 at top-left
+        combined_img.paste(image2, (len(morph_data1) * 2, 0)) # Paste img2 next to img1
 
-    combined_img.paste(image2, (0, len(morph_data1))) # Paste img1 at top-left
-    combined_img.paste(morph2 , (len(morph_data1), len(morph_data1))) # Paste img1 at top-left
-    combined_img.paste(image1, (len(morph_data1) * 2, len(morph_data1))) # Paste img2 next to img1
+        combined_img.paste(image2, (0, len(morph_data1))) # Paste img1 at top-left
+        combined_img.paste(morph2 , (len(morph_data1), len(morph_data1))) # Paste img1 at top-left
+        combined_img.paste(image1, (len(morph_data1) * 2, len(morph_data1))) # Paste img2 next to img1
 
-    combined_img.show()
-    
-    combined_img.save("ImageMorph/output.png")
+        combined_img.show()
+        
+        combined_img.save("ImageMorph/output.png")
+    elif kind == 1:
+        morph1.save("ImageMorph/morph1.png")
+        morph1.show()
+    else:
+        morph2.save("ImageMorph/morph2.png")
+        
+
     
 def pair_opimtmally_and_resolve_conflicts(pool1,pool2,pairs):
     # compatibility_avg = 0
@@ -266,6 +303,7 @@ def pair_opimtmally_and_resolve_conflicts(pool1,pool2,pairs):
 
 def opimize_pairs_iterativly(pool1,pool2,pairs):
     
+    
     # Shuffle
   
     # n_pool1 = list()
@@ -325,33 +363,35 @@ def opimize_pairs_iterativly(pool1,pool2,pairs):
     for i in range(size**2):
         pairs.append([pool1[i],pool2[i]])
     
+    frame_chance = 20
     score = sum(compatibilites)
-    print(score," Before")
     swaps = list()
     with Bar('Processing...') as bar:  
         max = 2000000
+        
         bar.max = max
-        i = 0
+        interval = 0
         while(True):
             bar.next()
-      
-            i += 1
-            if i % max  == 0:
+            interval += 1
+            if interval % max == 0:
+        
                 bar.index = 0
                 new_score = 0
+                i = 0
+                # video.release()
                 for i in range(len(pairs)):
                     new_score += get_compatibility(pairs[i][0],pairs[i][1])
                 print("Improved by",(100 * (new_score - score))/(score + 1))
                 score = new_score
                 make_image()
                 
-            swap_size = np.random.randint(1)
-            
-            indexs = [[np.random.randint(swap_size,size - swap_size),np.random.randint(swap_size,size - swap_size)]
-                      ,[np.random.randint(swap_size,size - swap_size),np.random.randint(swap_size,size - swap_size)]]
+            swap_size = np.random.randint(1,5 + 1)
+            indexs = [[np.random.randint(0,size - swap_size + 1),np.random.randint(0,size - swap_size + 1)]
+                      ,[np.random.randint(0,size - swap_size + 1),np.random.randint(0,size - swap_size + 1)]]
             
             while indexs[0] == indexs[1]:
-                indexs[1] = [np.random.randint(swap_size,size - swap_size),np.random.randint(swap_size,size - swap_size)] # no swapimg same item
+                indexs[1] = [np.random.randint(0,size - swap_size + 1),np.random.randint(0,size - swap_size + 1)] # no swapimg same item
                 
             swaped_pairs = list()
             old_compatibilities = list()
@@ -361,7 +401,6 @@ def opimize_pairs_iterativly(pool1,pool2,pairs):
                 for y in range(swap_size):
                  #print((indexs[0]), x,(indexs[0]) + y)
                  pos = [[(indexs[0][0]) + x,(indexs[0][1]) + y],[(indexs[1][0]) + x,(indexs[1][1]) + y]] #
-                 #print(pos[0][1] + pos[0][0] * size,pos[0][1], " + ",  pos[0][0] * size)
                  
                  pairs_to_swap = [pairs[pos[0][1] + pos[0][0] * size],pairs[pos[1][1] + pos[1][0] * size]]
                  
@@ -372,14 +411,15 @@ def opimize_pairs_iterativly(pool1,pool2,pairs):
                  
                  new_compatibilities.append(get_compatibility(pairs_to_swap[0][0],pairs_to_swap[1][1]))
                  new_compatibilities.append(get_compatibility(pairs_to_swap[1][0],pairs_to_swap[0][1]))
-          
             if sum(new_compatibilities) > sum(old_compatibilities):
+                #  if interval % frame_chance == 0:
+                #     video_add_frame()
                  for x in range(swap_size):
                     for y in range(swap_size):
                         pos = [[(indexs[0][0]) + x,(indexs[0][1]) + y],[(indexs[1][0]) + x,(indexs[1][1]) + y]]
                         #print(x,y*size,len(swaped_pairs))
-                        pairs[pos[0][1] + pos[0][0] * size] = swaped_pairs[(x + y * swap_size)//2][0]
-                        pairs[pos[1][1] + pos[1][0] * size] = swaped_pairs[(x + y * swap_size)//2][1]
+                        pairs[pos[0][1] + pos[0][0] * size] = swaped_pairs[(x + y * swap_size)][0]
+                        pairs[pos[1][1] + pos[1][0] * size] = swaped_pairs[(x + y * swap_size)][1]
                         
     
     
